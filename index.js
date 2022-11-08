21eeafb954998410a12cf3f58ebbbb4460b8c99d
Nayon <nayonroy64@gmail.com>
2022-11-08 16:52:45 +0600

    enviroment varialbe setpu

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const cors = require('cors')
 const app = express()
 require("dotenv").config();
 
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xicrlbt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get("/", (req, res) => {
 	res.send("server is running")
